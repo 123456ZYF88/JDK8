b5b65ce40100bf64fce4506ea032cb103c863cdb
zhangyifan <15836784326@163.com>
2022-09-12 23:33:52 +0800

    Unicoded的换行符

---
+++ src/main/java/org/examle04/UnicodeTest.java
@@ -0,0 +1,29 @@
+package org.examle04;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author zhangyifan
+ * @version 8.0
+ * @description: \u000d编译
+ * @date 2022/9/12 22:15
+ */
+public class UnicodeTest {
+    public static void main(String[] args) {
+        String name ="张三";
+        // \u000d name ="李四";
+        System.out.println(name);
+    }
+    @Test
+    public void filet(){
+        try {
+            java.io.FileWriter f =new //\u000djava.\u000dio.\u000dFileWriter\u000d("t.txt");
+                    f.//\u000dwrite//\u000d("abcd");
+                  f.//\u000dclose//\u000d();
+          System.out.println("文件创建成功");
+            f.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
