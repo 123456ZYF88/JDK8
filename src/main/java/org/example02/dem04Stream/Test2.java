1857b20f438e5fb1fcd72f18f5b8a81a39b07404
zhangyifan <zhangyifan>
2023-04-04 16:42:52 +0800

    test

--- src/main/java/org/example02/dem04Stream/Test2.java
+++ src/main/java/org/example02/dem04Stream/Test2.java
@@ -9,7 +9,7 @@ import java.util.List;
 /**
  * @Author ZhangYiFan
  * @Date 2023/3/16 9:17
- * @description:
+ * @description: Stream常用方法测试
  * @Version 1.0
  */
 public class Test2 {
@@ -19,7 +19,7 @@ public class Test2 {
      * 该方法接收一个 Consumer 接口函数，会将每一个流元素交给该函数进行处理
      * void forEach(Consumer<? super T> action);
      */
-    public void testFoEach(){
+    public void testFoEach() {
         List<String> one = new ArrayList<>();
         Collections.addAll(one, "迪丽热巴", "宋远桥", "苏星河", "老子", "庄子", "孙子");
 
@@ -27,31 +27,77 @@ public class Test2 {
 //            System.out.println(s);
 //        });
 
-          //  one.stream().forEach(s -> System.out.println(s));
+        //  one.stream().forEach(s -> System.out.println(s));
 
         one.stream().forEach(System.out::println);
     }
 
-    @Test
+
     /**
      * long count();该方法返回一个long值代表元素个数。
      */
-    public void testCount(){
+    @Test
+    public void testCount() {
         List<String> one = new ArrayList<>();
         Collections.addAll(one, "迪丽热巴", "宋远桥", "苏星河", "老子", "庄子", "孙子");
         System.out.println(one.stream().count());
     }
 
-    @Test
+
     /**
      * Stream<T> filter(Predicate<? super T> predicate);
      * 该接口接收一个 Predicate 函数式接口参数（可以是一个Lambda或方法引用）作为筛选条件。
      */
-    public  void testFilter(){
+    @Test
+    public void testFilter() {
         List<String> one = new ArrayList<>();
         Collections.addAll(one, "迪丽热巴", "宋远桥", "苏星河", "老子", "庄子", "孙子");
+        one.stream().filter(s -> s.length() == 2).forEach(System.out::println);
+    }
 
-        one.stream().filter(s -> s.length()==2).forEach(System.out::println);
+    @Test
+    public void testLimit() {
+
+    }
+
+    @Test
+    public void testSkip() {
+
+    }
+
+    @Test
+    public void testMap() {
+
+    }
+
+    @Test
+    public void testSorted() {
+
+    }
+
+    @Test
+    public void testDistinct() {
+
+    }
+
+    @Test
+    public void testMatch() {
+
+    }
+
+    @Test
+    public void testfind() {
+
+    }
+
+    @Test
+    public void testMax() {
+
+    }
+
+    @Test
+    public void testMin() {
+
     }
 
 
